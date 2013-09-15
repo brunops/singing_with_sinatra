9d1eca78d7e6df9e0dfae4e8c916a4fb7ded7b98
Bruno Sanches <brunopsanches@gmail.com>
2013-09-15 12:50:24 -0700

    Include shotgun gem and create two more test routes

--- basic.rb
+++ basic.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
   'Sup!'
 end
+
+get '/hello' do
+  "Hello, World!"
+end
+
+get '/hello/:name' do
+  "Hello #{params[:name]}, dear friend."
+end
+
